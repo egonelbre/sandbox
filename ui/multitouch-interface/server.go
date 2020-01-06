dc562642ae2d8edc01a86f2913f8377656521334
Egon Elbre <egonelbre@gmail.com>
2020-01-06 12:55:17 +0200

    ui/multitouch-interface: go fmt

--- ui/multitouch-interface/server.go
+++ ui/multitouch-interface/server.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "flag"
-    "net/http"
-    "html/template"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-var addr = flag.String("addr", ":8000", "http service address") // Q=17, R=18
+var addr = flag.String("addr", "127.0.0.1:8000", "http service address") // Q=17, R=18
 
 const (
-    StaticDir = "static/"
+	StaticDir = "static/"
 )
 
 var (
-    mainTemplate, _ = template.ParseFiles("index.html")
-    errorTemplate, _ = template.ParseFiles("error.html")
+	mainTemplate, _  = template.ParseFiles("index.html")
+	errorTemplate, _ = template.ParseFiles("error.html")
 )
 
 func main() {
-    fmt.Println("Started on: ", *addr);
-    http.HandleFunc("/", errorHandler(handler))
-    http.HandleFunc("/static/", errorHandler(static))
-    log.Fatal(http.ListenAndServe(*addr, nil))
+	flag.Parse()
+	fmt.Println("Started on: ", *addr)
+	http.HandleFunc("/", errorHandler(handler))
+	http.HandleFunc("/static/", errorHandler(static))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
 func errorHandler(fn http.HandlerFunc) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        defer func() {
-            if e := recover(); e != nil {
-                w.WriteHeader(500)
-                errorTemplate.Execute(w, e)
-            }
-        }()
-        fn(w, r)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if e := recover(); e != nil {
+				w.WriteHeader(500)
+				errorTemplate.Execute(w, e)
+			}
+		}()
+		fn(w, r)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    mainTemplate.Execute(w, nil)
+	mainTemplate.Execute(w, nil)
 }
 
 func static(w http.ResponseWriter, r *http.Request) {
-    filename := r.URL.Path[8:]
-    http.ServeFile(w, r, StaticDir + filename)
+	filename := r.URL.Path[8:]
+	http.ServeFile(w, r, StaticDir+filename)
 }
